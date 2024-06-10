8a06d2664de3259186c69ead42cc190e2b4aabde
Rahul Bansal <rahulbansal24072004@gmail.com>
2024-06-10 21:22:50 +0530

    change in origin

--- index.js
+++ index.js
@@ -6,9 +6,17 @@ import router from './Routes/emailRoute.js';
 const app = express();
 
 dotenv.config();
-app.use(cors({
-  origin: '*',
-}));
+// app.use(cors({
+//   origin: '*',
+// }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(express.json());
 
 app.use('/email', router);
